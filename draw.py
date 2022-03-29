0aa0fd0ef8730447ab3b3467aa22e721921afa01
DEDSacram <80846885+DEDSacram@users.noreply.github.com>
2022-03-29 22:17:45 +0200

    Add files via upload

--- draw.py
+++ draw.py
@@ -9,10 +9,7 @@ import os
 r = 30
 basedir = "data/images/"
 images = ["Cool.png","pog.png","pepeppo.png","HAHAUDIETHANOSSNAP.png","sus.png","garbage.jpeg"] # to be changed
-imagepath = basedir + "pog.png"
-
-
-
+imagepath = basedir + "Cool.png"
 def detectlines(image):
     img = cv2.imread(image)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -36,85 +33,38 @@ def detectlines(image):
     for line in lines:
         for x1,y1,x2,y2 in line:
             cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
-  
-    # blur_gray = cv2.GaussianBlur(line_image,(kernel_size, kernel_size),1)
-
-
     img_erosion = cv2.erode(line_image, (kernel_size, kernel_size), iterations=1)
     edges = cv2.Canny(img_erosion, low_threshold, high_threshold)
-    
-
     # mask_image = np.copy(img) * 0
-
     contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # print(len(contours))
-    # cv2.drawContours(mask_image, contours, -1, (0,255,0), 3)
-    # cv2.imshow("HoughCirlces",	mask_image)
-    # cv2.waitKey()
-   
-    # print(len(contours))
-    # cv2.imshow("HoughCirlces",mask_image)
-    # cv2.waitKey()
-    # # points = []
-    # for line in lines:
-    #     for x1,y1,x2,y2 in line:
-    #         # points.append(((x1 + 0.0, y1 + 0.0), (x2 + 0.0, y2 + 0.0)))
-    #         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
     return contours
 
 def imageintoGraph(path):
-    # os.system(f"cd .. &&  python detect.py --weights runs/train/graphs/weights/last.pt --img 640 --conf 0.6 --source "+path+" --hide-labels")
-
+    os.system(f"cd .. &&  python detect.py --weights runs/train/graphs/weights/last.pt --img 640 --conf 0.6 --source "+path+" --hide-labels")
     prefix = "../"
-
     cords = [] # all detections
     centercords = [] # all detections
-
     nodes = []
     edges = []
-
+    linestopbot=[]
+    linecenter=[]
     lineedges = detectlines(prefix+path) # line Contours
-   
     planets	= cv2.imread(prefix+path)
     gray_img=cv2.cvtColor(planets,	cv2.COLOR_BGR2GRAY)
     img	= cv2.medianBlur(gray_img,	1)
-   
-    
     circles	= cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,30,param1=100,param2=30,minRadius=20,maxRadius=120)
     circles	= np.uint16(np.around(circles))
-
-  
     with open("alldetections.txt") as f:
         lines = f.readlines()
         for x in lines:
             x = x.rstrip() 
             cords.append([int(float(i)) for i in x.split(',')])
-
     for cord in cords:
         width = (cord[2]-cord[0])
         height = (cord[1]-cord[3])
         centerx = cord[0] + int(width/2)
         centery = cord[1] - int(height/2)
-     
         centercords.append([centerx,centery])
-    # (x - center_x)² + (y - center_y)² < radius²
-    # for	i in circles[0,:]:         
-    #                 for cord in centercords:
-    #                     if((math.pow((cord[0] - i[0]), 2) + math.pow((cord[1] - i[1]), 2)) < math.pow(i[2],2)):
-    #                         # print(True)
-    #                         nodes.append([cord[0],cord[1]])
-    #                     else:
-    #                         # print(False)
-    #                         if [cord[0],cord[1]] not in edges:
-    #                             edges.append([cord[0],cord[1]])
-    #                 #	draw	the	outer	circle
-    #                 cv2.circle(planets,(i[0],i[1]),i[2],(0,128,255),6)
-    #                 # #	draw	the	center	of	the	circle
-    #                 # cv2.circle(planets,(i[0],i[1]),2,(0,0,255),3)
-
-
-
     # Check if in circle then append
     for cord in centercords:
         isin = False
@@ -126,14 +76,12 @@ def imageintoGraph(path):
             nodes.append([cord[0],cord[1]])
         else:
             edges.append([cord[0],cord[1]])
-
-    # Draw
-    # (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
     for a in nodes:
          cv2.circle(planets,(a[0],a[1]),20,(0,0,255),3)
     for b in edges:
          cv2.circle(planets,(b[0],b[1]),20,(255,0,128),3)
     
+    cv2.drawContours(planets, lineedges,-1, (0,255,0), 3)
     for crt in lineedges:
         leftmost = tuple(crt[crt[:,:,0].argmin()][0])
         rightmost = tuple(crt[crt[:,:,0].argmax()][0])
@@ -141,12 +89,17 @@ def imageintoGraph(path):
         # bottommost = tuple(crt[crt[:,:,1].argmax()][0])
         # cv2.circle(planets,(topmost[0],topmost[1]),10,(0,128,64),6)
         # cv2.circle(planets,(bottommost[0],bottommost[1]),10,(0,256,64),6)
-        cv2.circle(planets,(rightmost[0],rightmost[1]),10,(256,0,64),6)
-        cv2.circle(planets,(leftmost[0],leftmost[1]),10,(0,0,256),6)
+        center = [int((rightmost[0]+leftmost[0])/2),int((rightmost[1]+leftmost[1])/2)]
+        cv2.circle(planets,(rightmost[0],rightmost[1]),4,(64,64,64),6)
+        cv2.circle(planets,(leftmost[0],leftmost[1]),4,(0,0,256),6)
+        cv2.circle(planets,(int((rightmost[0]+leftmost[0])/2),int((rightmost[1]+leftmost[1])/2)),4,(128,64,0),6)
+        linestopbot.append([[leftmost[0],leftmost[1],rightmost[0],rightmost[1]]])
+        linecenter.append(center)
 
-        cv2.drawContours(planets, lineedges,-1, (0,255,0), 3)
-        # cv2.drawContours(planets, crt,0, (0,255,0), 8)
     
+    print(linestopbot)
+    print(linecenter)
+        # cv2.drawContours(planets, crt,0, (0,255,0), 8) 
     cv2.imshow("HoughCirlces",	planets)
     cv2.waitKey()
     # cv2.destroyAllWindows()
@@ -185,8 +138,6 @@ def floydWarshall(graph):
                                  )
     graphintoText(dist)
     return dist
-
-
 WIDTH, HEIGHT = 1980, 1080
 screen = Screen()
 screen.setup(WIDTH + 4, HEIGHT + 8)  # fudge factors due to window borders & title bar
@@ -226,8 +177,6 @@ def drawnodes(nodes,points):
         goto(points[i][0],points[i][1])
         pendown()
         circle(r)
-
-
 # PROBLEMS WHEN TOO CLOSE TO X,Y axis intersections of the circle. BECAUSE OF REVERSING SIDES LINE GOES THROUGH THE NODE 
 def drawconnectionlines(points,graph,offset,add,distances):
     nodeint = 0
@@ -275,8 +224,6 @@ def drawconnectionlines(points,graph,offset,add,distances):
             i+=1
         i=0
         nodeint+=1
-
-
 
 imageintoGraph(imagepath)
 floydWarshall(graph)
